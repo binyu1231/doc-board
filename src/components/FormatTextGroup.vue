391ffd6d056d607599b7a2ed4ed6945ebd319654
wangbinyu <binyu1231@gmail.com>
2023-01-31 09:12:56 +0800

    perf: format text

--- src/components/FormatTextGroup.vue
+++ src/components/FormatTextGroup.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue'
+import { onMounted } from 'vue'
 import { useStorage } from '@vueuse/core'
 enum FormatPattern {
     regexp, string
@@ -28,17 +28,23 @@ onMounted(() => {
 <template>
     <div class="relative group" v-for="(item, i) in state.formatText" :key="i">
         <FormatText :matchList="item.list" @change="(item: any) => changeItem(item, i)" />
-        <div class="absolute right-1 bottom-1 group-hover:block hidden cursor-pointer" @click="removeFormat(i)">x</div>
+        <div class="absolute right-0 -bottom-1 group-hover:block hidden cursor-pointer" @click="removeFormat(i)">
+            <i-akar-icons-circle-x />
+        </div>
         <hr>
     </div>
 
     <div class="flex gap-4">
-        <div class="format-text-group-button text-2xl" @click="addFormat">
-            +
+        <div class="format-text-group-button text-xl ftg-border-color rounded-sm flex items-center justify-center py-1" @click="addFormat">
+            <i-akar-icons-plus />
         </div>
     </div>
 </template>
 <style lang="postcss">
+.ftg-border-color {
+    @apply border-gray-400 dark:border-gray-500 border-1 rounded-sm text-gray-500 dark:text-gray-400;
+}
+
 .format-text-group-button {
     @apply border-1 rounded-sm flex justify-center items-center cursor-pointer flex-1;
 }
